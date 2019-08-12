c8cd412bf21f8461c6ecaa4a8e1943190da0eff4
Daniel Palmer <daniel@0x0f.com>
2019-08-12 20:20:43 +0900

    Make rescue image smaller. Carve out 256KB of the rescue image area for some RW space. Sync linux config. Add script for setting up the RW space.

---
+++ br2breadbee/package/rwsetup/rwsetup.mk
@@ -0,0 +1,16 @@
+################################################################################
+#
+# rwsetup
+#
+################################################################################
+
+RWSETUP_VERSION = 1.0
+RWSETUP_SITE = $(BR2_EXTERNAL_BREADBEE_PATH)/package/rwsetup
+RWSETUP_SITE_METHOD = local
+RWSETUP_LICENSE = GPLv3
+
+define RWSETUP_INSTALL_INIT_SYSV
+	$(INSTALL) -D -m 755 $(@D)/S22rwsetup $(TARGET_DIR)/etc/init.d/S22rwsetup
+endef
+
+$(eval $(generic-package))
