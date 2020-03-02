da4289416fea8747871fad8eb482641d145b3422
Daniel Palmer <daniel@0x0f.com>
2020-03-02 21:46:35 +0900

    Link the IR overlay to the required userland tools

--- br2breadbee/package/breadbee-overlays/breadbee-overlays.mk
+++ br2breadbee/package/breadbee-overlays/breadbee-overlays.mk
@@ -7,7 +7,7 @@
 BREADBEE_OVERLAYS_VERSION = 1.0
 BREADBEE_OVERLAYS_SITE = $(BR2_EXTERNAL_BREADBEE_PATH)/package/breadbee-overlays
 BREADBEE_OVERLAYS_SITE_METHOD = local
-BREADBEE_OVERLAYS_LICENSE = BSD-3-Clause
+BREADBEE_OVERLAYS_LICENSE = GPLv3
 BREADBEE_OVERLAYS_INSTALL_IMAGES = YES
 BREADBEE_OVERLAYS_DEPENDENCIES = host-dtc
 
@@ -15,6 +15,7 @@ define BREADBEE_OVERLAYS_INSTALL_DTB_OVERLAYS
         for ovl in  $(@D)/*.dts; do \
 		$(HOST_DIR)/bin/dtc -@ -a 4 -I dts -O dtb -o $(@D)/$$(basename "$$ovl" .dts).dtb $${ovl}; \
         done
+
 	rm -f $(BINARIES_DIR)/breadbee-overlays/fdtlist
 	rm -f $(BINARIES_DIR)/breadbee-overlays/configlist
 	off=587235328; for ovldtb in  $(@D)/*.dtb; do \
